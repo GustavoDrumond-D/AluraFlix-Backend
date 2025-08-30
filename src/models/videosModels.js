d5c7249cdeaad605e0482deba383a2b9b999b168
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-08-30 20:33:29 -0300

    refactor: Readicionando '_id' ao modelo

--- src/models/videosModels.js
+++ src/models/videosModels.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    _id: mongoose.Schema.Types.ObjectId,
+    _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     titulo: { type: String, required: true, min: 8, max: 50 },
     descricao: { type: String, required: true },
     url: { type: String, required: true , unique: true },
