cb665ab2cc8e4c0a7caa952d0503548d6752bc60
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-08-30 20:00:14 -0300

    fix: Corrigindo erro no controlador de videos

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,7 +1,9 @@
+import Video from "../models/videosModels.js";
+
 class   videosController {
     static  async listarVideos(req, res) {
         try {
-            const videos = await videos.find();
+            const videos = await Video.find();
             res.status(200).json(videos);
         } catch (error) {
             res.status(500).send({ message: 'Erro ao listar vídeos' });
