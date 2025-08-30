09e00ab60686548dc519f0428dce4a5b16acac50
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-08-30 20:01:13 -0300

    refactor: refatorando arquivo 'videosModels'

--- src/models/videosModels.js
+++ src/models/videosModels.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const videosSchema = new mongoose.Schema({
+const videoSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     titulo: { type: String, required: true, min: 8, max: 50 },
     descricao: { type: String, required: true },
     url: { type: String, required: true , unique: true },
 });
 
-const videos = mongoose.model('videos', videosSchema);
+const Video = mongoose.model('Video', videoSchema);
 
-export default videos;
+export default Video;
