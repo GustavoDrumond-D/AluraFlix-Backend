02b4746b2fe69a1033ffaf3a9caf37c4841580ef
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-08-30 21:57:20 -0300

    feat: Adicionando controlador para cadastrar videos

--- src/controllers/videosController.js
+++ src/controllers/videosController.js
@@ -1,12 +1,21 @@
 import Video from "../models/videosModels.js";
 
-class   videosController {
+class videosController {
     static  async listarVideos(req, res) {
         try {
             const videos = await Video.find({});
             res.status(200).json(videos);
         } catch (error) {
             res.status(500).send({ message: 'Erro ao listar vídeos' });
+        }
+    }
+
+    static async cadastrarVideo(req, res) {
+        try {
+            const novoVideo = await Video.create(req.body);
+            res.status(201).json(novoVideo);
+        } catch (error) {
+            res.status(500).send({ message: 'Erro ao cadastrar vídeo' });
         }
     }
 }
