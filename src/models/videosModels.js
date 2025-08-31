d84a6fc372d5a8e9dfbee052b5913bf80c0f9648
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-08-30 21:55:53 -0300

    feat: removendo campo de versão ao criar um video

--- src/models/videosModels.js
+++ src/models/videosModels.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+import mongoose, { version } from "mongoose";
 
 const videoSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, auto: true },
     titulo: { type: String, required: true, min: 8, max: 50 },
     descricao: { type: String, required: true },
     url: { type: String, required: true , unique: true },
-});
+    }, {versionKey: false}
+);
 
 const Video = mongoose.model('Video', videoSchema);
 
