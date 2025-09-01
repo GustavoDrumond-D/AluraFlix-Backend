6b9a695ff41a976d6b2047bb171775f28bb5ca39
Gustavo Drumond <gdrumondduarte@gmail.com>
2025-09-01 13:17:39 -0300

    feat: Criando endpoint GET, para encontrar um video por id

--- src/routes/videosRoutes.js
+++ src/routes/videosRoutes.js
@@ -2,9 +2,13 @@ import express from 'express';
 import videosController from '../controllers/videosController.js';
 
 const router = express.Router();
+//endpoint /videos
 
+// Rota para listar vídeos
 router.get('/videos', videosController.listarVideos);
-
+// Rota para listar vídeo por ID
+router.get('/videos/:id', videosController.listarVideosPorId)
+// Rota para cadastrar vídeo
 router.post('/videos', videosController.cadastrarVideo);
 
 export default router;
